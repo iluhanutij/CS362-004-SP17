b0073796e80ce196f80e5a5fa2ff08743f2244ea
Ilya <ilya.zhimanov@gmail.com>
2017-05-01 23:35:54 -0700

    finished the quit

---
+++ projects/zhimanoi/RandomTesting/randomstring.c
@@ -0,0 +1,12 @@
+This is text file that outline how I developed my solution and how it works:
+
+First I looked at what the code does. Then I compiled it and ran the program.
+It was an infinite loop. I needed to fill in the two funciont that will help
+me terminate the program so that the loop doesn't continue repeating forever.
+I needed to use inputChar() funtion to input a random character in a certain
+range. The second function that needed to be filled in was inputString. 
+This function goes through the loop of randomly generated character or a string. I chose the lengths of the
+string to be 6. Then I get a random ASCII character in the certain range.
+Now when I run this program and it terminates with:
+Iteration 5796251: c = f, s = reset, state = 9
+it took about a minute to finish running.
